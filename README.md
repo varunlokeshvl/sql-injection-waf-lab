728c039dea7633a0779a4875109df825f96936bb
Varun Lokesh <varunlokesh@Varuns-MacBook-Air.local>
2026-03-13 11:12:44 +0530

    Project done

---
+++ README.md
@@ -0,0 +1,18 @@
+SQL Injection WAF Lab
+
+EC2 Public IP:
+http://13.x.x.x
+
+Vulnerable Page:
+http://13.x.x.x/page1.php
+
+Protected Page:
+http://13.x.x.x/page2.php
+
+Technologies:
+AWS EC2
+Apache
+MySQL
+PHP
+ModSecurity WAF
+OWASP CRS
---
+++ page1.php
@@ -0,0 +1,19 @@
+<?php
+$conn = mysqli_connect("localhost","root","","login_db");
+
+$username = $_POST['username'];
+
+$query = "SELECT * FROM users WHERE username='$username'";
+$result = mysqli_query($conn,$query);
+
+if(mysqli_num_rows($result)>0){
+echo "Login Successful";
+}else{
+echo "Login Failed";
+}
+?>
+
+<form method="POST">
+<input type="text" name="username" placeholder="Enter username">
+<button type="submit">Login</button>
+</form>
